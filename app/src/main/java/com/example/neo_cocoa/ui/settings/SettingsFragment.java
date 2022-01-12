e91dd81ae397e3deb8ff61981fbd57f27b596aa5
Keisuke Magara <61485115+Keisuke-Magara@users.noreply.github.com>
2022-01-12 22:53:59 +0900

    Save changes (1/12)

--- app/src/main/java/com/example/neo_cocoa/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/neo_cocoa/ui/settings/SettingsFragment.java
@@ -1,4 +1,4 @@
-package com.example.neo_cocoa.ui.notifications;
+package com.example.neo_cocoa.ui.settings;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,27 +12,26 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.neo_cocoa.R;
-import com.example.neo_cocoa.databinding.FragmentNotificationsBinding;
+import com.example.neo_cocoa.databinding.FragmentSettingsBinding;
 
-public class NotificationsFragment extends Fragment {
+public class SettingsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
-    private FragmentNotificationsBinding binding;
+    private SettingsViewModel settingsViewModel;
+    private FragmentSettingsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        settingsViewModel =
+                new ViewModelProvider(this).get(SettingsViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        //final TextView textView = binding.textNotifications;
+        settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //textView.setText(s);
             }
         });
         return root;
