b7c5338f7b51f999d18970f50555ca5d88ff1b86
Keisuke-Magara <61485115+Keisuke-Magara@users.noreply.github.com>
2022-02-02 12:46:39 +0900

    create quit button dialog.

---
+++ app/src/main/java/com/example/neo_cocoa/quit.java
@@ -0,0 +1,7 @@
+package com.example.neo_cocoa;
+
+public class quit {
+    public quit() {
+        System.out.println("Quit to use this app!");
+    }
+}
--- app/src/main/java/com/example/neo_cocoa/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/neo_cocoa/ui/settings/SettingsFragment.java
@@ -1,10 +1,13 @@
 package com.example.neo_cocoa.ui.settings;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -17,6 +20,9 @@ import androidx.fragment.app.Fragment;
 import com.example.neo_cocoa.R;
 import com.example.neo_cocoa.appSettings;
 import com.example.neo_cocoa.databinding.FragmentSettingsBinding;
+import com.example.neo_cocoa.quit;
+
+import org.w3c.dom.Text;
 
 public class SettingsFragment extends Fragment implements CompoundButton.OnCheckedChangeListener {
 
@@ -32,8 +38,30 @@ public class SettingsFragment extends Fragment implements CompoundButton.OnCheck
 
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
         TextView version_number = view.findViewById(R.id.settings_appversion);
+        getAppVersion(version_number);
         Switch s = view.findViewById(R.id.settings_bgnotification_switch);
-        // get app version.
+        Button quit = view.findViewById(R.id.settings_quit_button);
+        Button share = view.findViewById(R.id.settings_share_button);
+
+        s.setOnCheckedChangeListener(this);
+        s.setChecked(appsettings.getBgNotif());
+        quit.setOnClickListener(quit_listener);
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        appsettings.setBgNotif(isChecked);
+    }
+
+    // get app version.
+    public boolean getAppVersion (TextView version_number) {
         try {
             // get Java package name.
             // android.content.Context#getPackageName
@@ -53,21 +81,27 @@ public class SettingsFragment extends Fragment implements CompoundButton.OnCheck
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
             version_number.setText("取得できませんでした.");
+            return false;
         }
-
-        s.setOnCheckedChangeListener(this);
-        s.setChecked(appsettings.getBgNotif());
-        return view;
+    return true;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
+    private View.OnClickListener quit_listener = new View.OnClickListener() {
+        public void onClick(View view) {
+            System.out.println("Clicked!");
+            AlertDialog.Builder ad = new AlertDialog.Builder(getActivity());
+            ad.setTitle(getString(R.string.settings_quit_ad_title));
+            ad.setMessage(getString(R.string.settings_quit_ad_descripstion));
+            ad.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    quit quit = new quit();
+                }
+            });
+            ad.setNegativeButton(getString(R.string.ng), null);
+            ad.show();
+        }
+    };
 
-    @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        appsettings.setBgNotif(isChecked);
-    }
+
 }
