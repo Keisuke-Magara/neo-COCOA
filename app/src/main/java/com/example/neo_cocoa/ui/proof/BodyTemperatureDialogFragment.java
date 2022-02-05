f11fce82941b789462940907c75e3ca4e6ca45f6
g2021060 <g2021060@tcu.ac.jp>
2022-02-05 21:04:50 +0900

    Merge remote-tracking branch 'origin/add_proof' into add_proof

--- app/src/main/java/com/example/neo_cocoa/ui/proof/BodyTemperatureDialogFragment.java
+++ app/src/main/java/com/example/neo_cocoa/ui/proof/BodyTemperatureDialogFragment.java
@@ -2,40 +2,33 @@ package com.example.neo_cocoa.ui.proof;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.icu.text.UnicodeSetIterator;
 import android.os.Bundle;
 import android.text.InputType;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.DialogFragment;
 
 import com.example.neo_cocoa.R;
-import com.example.neo_cocoa.appProof;
 
 public class BodyTemperatureDialogFragment extends DialogFragment {
-    private appProof appproof;
-    private TextView btData;
-    public BodyTemperatureDialogFragment(appProof ap, TextView btd){
-        this.appproof = ap;
-        this.btData = btd;
+    private ProofFragment proofFragment;
+    private EditText btEditText;
+
+    public BodyTemperatureDialogFragment(ProofFragment pf){
+        this.proofFragment = pf;//
     }
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        final EditText editText = new EditText(getActivity());
-        // Use the Builder class for convenient dialog construction
-//        Context string;
-        editText.setHint(R.string.proof_dialog_message_body_temperature);
-        editText.setInputType( InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        btEditText = new EditText(getActivity());
+        btEditText.setHint(R.string.proof_dialog_message_body_temperature);
+        btEditText.setInputType( InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.proof_title_body_temperature);
-//        builder.setMessage(R.string.proof_dialog_message_body_temperature);
         builder.setPositiveButton(R.string.ok, new DialogButtonClickListener());
         builder.setNegativeButton(R.string.ng, new DialogButtonClickListener());
-        builder.setView(editText);
+        builder.setView(btEditText);
         //ダイアログオブジェクトを生成し、リターン。
         AlertDialog dialog = builder.create();
         return dialog;
@@ -44,16 +37,19 @@ public class BodyTemperatureDialogFragment extends DialogFragment {
     private class DialogButtonClickListener implements DialogInterface.OnClickListener {
         @Override
         public void onClick(DialogInterface dialog, int which) {
-            String bodyTemperature;
             String msg = "";
             switch(which) {
                 case DialogInterface.BUTTON_POSITIVE:
-                    appproof.setBodyTemperature("30.0度");
-                    btData.setText(appproof.getBodyTemperature());
-                    msg = "体温を登録しました。";
+                    if(btEditText.length() != 0) {
+                        proofFragment.setBodyTemperature(Float.valueOf(btEditText.getText().toString()));
+                        proofFragment.refreshProofFragment();
+                        msg = "体温を登録しました";
+                    }else {
+                        msg = "入力に間違いがあります";
+                    }
                     break;
                 case DialogInterface.BUTTON_NEGATIVE:
-                    msg = "登録をキャンセルしました。";
+                    msg = "登録をキャンセルしました";
                     break;
             }
             Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
--- app/src/main/java/com/example/neo_cocoa/ui/proof/ProofFragment.java
+++ app/src/main/java/com/example/neo_cocoa/ui/proof/ProofFragment.java
@@ -1,51 +1,59 @@
 package com.example.neo_cocoa.ui.proof;
 
-import android.app.AlertDialog;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 
 import com.example.neo_cocoa.R;
-import com.example.neo_cocoa.appProof;
+import com.example.neo_cocoa.AppProof;
 import com.example.neo_cocoa.databinding.FragmentProofBinding;
 
 public class ProofFragment extends Fragment {
     private FragmentProofBinding binding;
-    private appProof appproof;
-    private Button button_bt;
-    private Button button_vac;
-    private Button button_share;
+    private AppProof appProof;
+    private ProofFragment proofFragment;
+    private Button buttonBt;
+    private Button buttonVac;
+    private Button buttonShare;
+
     private TextView btData;
     private TextView vacData;
 
+    private ImageView ivContact;
+    private ImageView ivMasculine;
+    private ImageView ivBodyTemperature;
+    private ImageView ivVaccine;
+    private View view;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        proofFragment = this;
+        appProof = new AppProof(this.getActivity());
 
-        appproof = new appProof(this.getActivity());
         binding = FragmentProofBinding.inflate(inflater, container, false);
 
-        View view = inflater.inflate(R.layout.fragment_proof, container, false);
+        view = inflater.inflate(R.layout.fragment_proof, container, false);
 
-        //データの表示
-        btData = (TextView)view.findViewById(R.id.proof_data_body_temperature);
-        btData.setText(appproof.getBodyTemperature());
-
+        //画面の表示データを更新
+        refreshProofFragment();
         //体温入力ボタンのリスナを設定
-        button_bt = (Button)view.findViewById(R.id.proof_button_body_temperature);
-        button_bt.setOnClickListener(button_bt_listener);
-        //ワクチン接種回数の入力
-        button_vac = (Button)view.findViewById(R.id.proof_button_vaccine);
-        button_vac.setOnClickListener(button_vac_listener);
+        buttonBt = (Button)view.findViewById(R.id.proof_button_body_temperature);
+        buttonBt.setOnClickListener(button_bt_listener);
+        //ワクチン接種回数入力ボタンのリスナを設定
+        buttonVac = (Button)view.findViewById(R.id.proof_button_vaccine);
+        buttonVac.setOnClickListener(button_vac_listener);
 
         return view;
     }
@@ -53,17 +61,67 @@ public class ProofFragment extends Fragment {
     //体温入力ボタンのリスナ
     Button.OnClickListener button_bt_listener = new Button.OnClickListener() {
         public void onClick(View view) {
-            BodyTemperatureDialogFragment dialogFragment = new BodyTemperatureDialogFragment(appproof, btData);
+            BodyTemperatureDialogFragment dialogFragment = new BodyTemperatureDialogFragment(proofFragment);
             dialogFragment.show(getActivity().getSupportFragmentManager(), "BodyTemperatureDialogFragment");
         }
     };
     //ワクチン接種回数のリスナ
     Button.OnClickListener button_vac_listener = new Button.OnClickListener() {
         public void onClick(View view) {
-            VaccineDialogFragment dialogFragment = new VaccineDialogFragment();
+            VaccineDialogFragment dialogFragment = new VaccineDialogFragment(proofFragment);
             dialogFragment.show(getActivity().getSupportFragmentManager(), "VaccineDialogFragment");
         }
     };
+    public void setBodyTemperature(Float bodyTemperature) {
+        appProof.setBodyTemperature(bodyTemperature);
+    }
+    public void setNumOfVaccine(Integer numOfVaccine) {
+        appProof.setNumOfVaccine(numOfVaccine);
+    }
+    public Integer getNumOfVaccine() {
+        return appProof.getNumOfVaccine();
+    }
+    public void setVaccineDate(Integer vaccineDate) {
+        appProof.setVaccineDate(vaccineDate);
+    }
+
+    //Fragmentをの情報を更新
+    public void refreshProofFragment() {
+        //データの表示
+        btData = (TextView)view.findViewById(R.id.proof_data_body_temperature);
+        btData.setText(String.valueOf(appProof.getBodyTemperature()) + "度");
+
+        vacData = (TextView)view.findViewById(R.id.proof_data_num_of_vaccine);
+        vacData.setText(appProof.getNumOfVaccine() + "回(" +
+                appProof.getVaccineDate()/10000 + "/" +
+                (appProof.getVaccineDate()/100)%100 + "/" +
+                appProof.getVaccineDate()%100 + ")");
+
+        //アイコンの表示
+        Resources res = getResources();
+        Drawable okIcon = ResourcesCompat.getDrawable(res, R.drawable.ic_proof_ok, null);
+        Drawable ngIcon = ResourcesCompat.getDrawable(res, R.drawable.ic_proof_ng, null);
+        ivContact = (ImageView) view.findViewById(R.id.proof_icon_num_of_contacts);
+        ivMasculine = (ImageView) view.findViewById(R.id.proof_icon_num_of_masculine);
+        ivBodyTemperature = (ImageView) view.findViewById(R.id.proof_icon_body_temperature);
+        ivVaccine = (ImageView) view.findViewById(R.id.proof_icon_num_of_vaccine);
+
+        ivContact.setImageDrawable(ngIcon);
+        ivMasculine.setImageDrawable(ngIcon);
+        if(appProof.getBodyTemperature() < 37.0F) {
+            ivBodyTemperature.setImageDrawable(okIcon);
+        }else {
+            ivBodyTemperature.setImageDrawable(ngIcon);
+        }
+
+        if(appProof.getNumOfVaccine() >= 2) {
+            ivVaccine.setImageDrawable(okIcon);
+        }else {
+            ivVaccine.setImageDrawable(ngIcon);
+        }
+
+
+    }
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
